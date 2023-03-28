cbe3b4f22c04fc392479ad374c780485c6a03ef3
carlos calderon <cdcalderon@gmail.com>
2023-03-27 22:07:36 -0500

    update CurveToken to fulfil linear curve price calculation

--- contracts/CurveToken.sol
+++ contracts/CurveToken.sol
@@ -2,67 +2,87 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
-//import "@openzeppelin/contracts/token/ERC1363/IERC1363Receiver.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IERC1363Receiver.sol";
 
 contract CurveToken is ERC777, IERC1363Receiver, Ownable {
     uint256 public tokenPrice;
     uint256 public reserve;
+    uint256 public constant INITIAL_TOKEN_PRICE = 1 ether;
+    uint256 public constant TOKEN_PRICE_INCREMENT = 1 ether;
 
     constructor(
         uint256 initialSupply,
-        address[] memory defaultOperators,
-        uint256 _tokenPrice
+        address[] memory defaultOperators
     ) ERC777("MyToken", "MTK", defaultOperators) {
         _mint(msg.sender, initialSupply, "", "");
-        tokenPrice = _tokenPrice;
+        tokenPrice = INITIAL_TOKEN_PRICE;
     }
 
     function buyTokens(uint256 amount) public payable {
-        uint256 totalPrice = amount * tokenPrice;
+        require(amount > 0, "Amount must be greater than zero");
+
+        uint256 totalPrice = calculateTotalPrice(amount);
         require(msg.value >= totalPrice, "Insufficient payment");
 
         reserve += msg.value;
         _mint(msg.sender, amount, "", "");
+        tokenPrice = calculateTokenPrice(totalPrice);
     }
 
     function sellTokens(uint256 amount) public {
-        uint256 payment = amount * tokenPrice;
+        require(amount > 0, "Amount must be greater than zero");
+
+        uint256 payment = calculatePayment(amount);
         require(payment <= reserve, "Insufficient reserve");
 
         reserve -= payment;
         _burn(msg.sender, amount, "", "");
         payable(msg.sender).transfer(payment);
+        tokenPrice = calculateTokenPrice(payment);
+    }
+
+    function calculateTotalPrice(uint256 amount) private view returns (uint256) {
+        return ((2 * amount * INITIAL_TOKEN_PRICE) + ((amount - 1) * TOKEN_PRICE_INCREMENT)) / 2;
+    }
+
+    function calculatePayment(uint256 amount) private view returns (uint256) {
+        return ((2 * amount * tokenPrice) - ((amount - 1) * TOKEN_PRICE_INCREMENT)) / 2;
+    }
+
+    function calculateTokenPrice(uint256 totalPrice) private view returns (uint256) {
+        return ((2 * totalPrice) - ((totalSupply() - 1) * TOKEN_PRICE_INCREMENT)) / (2 * totalSupply());
     }
 
     function onTransferReceived(
         address /* operator */,
-        address /* from */,
+        address from,
         uint256 amount,
         bytes memory data
     ) public virtual override returns (bytes4) {
-        if (msg.sender == address(this)) {
-            // Token sale and buyback
-            return this.onTokensReceived.selector;
-        } else {
-            // Token transfer to contract
-            require(data.length == 0, "Data not supported");
-            _mint(address(this), amount, "", "");
-            return this.onTokensReceived.selector;
-        }
-    }
-
-    function onTokensReceived(
-        address /* operator */,
-        address /* from */,
-        uint256 /* amount */,
-        bytes memory data,
-        bytes memory /*operatorData*/
-    ) public virtual returns (bytes4) {
         require(msg.sender == address(this), "Invalid token");
         require(data.length == 0, "Data not supported");
 
+        uint256 payment = calculatePayment(amount);
+        reserve += payment;
+        _mint(from, amount, "", "");
+        return this.onTransferReceived.selector;
+    }
+
+    function onTokensReceived(
+        address operator,
+        address from,
+        uint256 amount,
+        bytes memory data,
+        bytes memory /*operatorData*/
+    ) public returns (bytes4) {
+        require(msg.sender == address(this), "Invalid token");
+        require(data.length == 0, "Data not supported");
+
+        uint256 payment = calculatePayment(amount);
+        reserve -= payment;
+        _burn(operator, amount, "", "");
+        payable(from).transfer(payment);
         return this.onTokensReceived.selector;
     }
 
